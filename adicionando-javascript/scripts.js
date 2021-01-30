7ea062627d94409cd9d3a633bd6ebfb9b3d49fcb
José <jose.ulombe@hotmail.com>
2021-01-30 07:13:09 +0100

    add scripts

--- adicionando-javascript/scripts.js
+++ adicionando-javascript/scripts.js
@@ -11,3 +11,13 @@ const Modal = {
         document.querySelector('.modal-overlay').classList.remove('active')
     }
 }
+
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions") || [])
+    },
+
+    set(transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+    }
+}
