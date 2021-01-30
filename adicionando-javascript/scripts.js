90c14586c641b71e430cf0a6cef29bb36dd33051
José <jose.ulombe@hotmail.com>
2021-01-30 08:02:07 +0100

    add scripts

--- adicionando-javascript/scripts.js
+++ adicionando-javascript/scripts.js
@@ -104,3 +104,33 @@ const DOM = {
         DOM.transactionsContainer.innerHTML = ""
     }
 }
+
+
+
+const Utils = {
+    formatAmount(value) {
+        value = Number(value.replace(/\,\./g, "")) * 100
+
+        return value
+    },
+
+    formatDate(date) {
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocalString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
